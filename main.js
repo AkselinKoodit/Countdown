d8274413b5defbe7e8a32798e8cf2ede79d87f5e
Akseli <s2100140@edu.bc.fi>
2021-02-20 11:07:44 +0200

    first gitHub commit for countdown application(working)

--- main.js
+++ main.js
@@ -48,13 +48,17 @@ const showMenuFunction = () => {
 
 menuBtn.addEventListener("click", showMenuFunction);
 
+//---------------------------------------------------------------------
+
 //Let's count to that baby!;
+
 let babyDue = new Date(2021, 2, 2, 12);
 let babyDueDate = new Date(2021, 2, 2, 12).getTime();
 
 let interval = setInterval(function () {
   let now = new Date();
   let currentTime = new Date().getTime();
+
   let difference = babyDueDate - currentTime;
 
   let days = Math.floor(difference / (1000 * 60 * 60 * 24));
@@ -72,49 +76,55 @@ let interval = setInterval(function () {
     minutes +
     " minutes and " +
     seconds +
-    " seconds until the birth of my daughter";
+    " seconds until the birth of my daughter. That includes " +
+    (days - Math.floor(days / 7) * 2 + " weekdays.");
   document.getElementById("dueDate").innerHTML = babyDue;
   document.getElementById("currentTime").innerHTML = now;
 });
 
-// let timeToBabyInDays = (babyDueDate - currentTime) / 1000 / 60 / 60 / 24;
-// console.log("Laskettu aika on: " + babyDueDate);
-// console.log(`laskettuun aikaan on  ${Math.round(timeToBabyInDays)} päivää :)`);
-// let timeToBabyHours = timeToBabyInDays * 24 - 13 * 24;
-// console.log(` ${Math.round(timeToBabyHours)} tuntia`);
-// console.log(
-//   "Milliseconds to the due date from Jan 1 1970: " + babyDueDate.getTime()
-// );
-// console.log("The same in minutes: " + babyDueDate.getTime() / 1000 / 60);
-// console.log(
-//   "And current time in minutes: " + currentTime.getTime() / 1000 / 60
-// );
-// console.log(
-//   "Duedate-today = " +
-//     (babyDueDate.getTime() / 1000 / 60 - currentTime.getTime() / 1000 / 60) +
-//     " minutes."
-// );
-// console.log(
-//   "And in hours: " +
-//     (babyDueDate.getTime() / 1000 / 60 / 60 -
-//       currentTime.getTime() / 1000 / 60 / 60)
-// );
-// console.log(
-//   "Aand in days: " +
-//     (babyDueDate.getTime() / 1000 / 60 / 60 / 24 -
-//       currentTime.getTime() / 1000 / 60 / 60 / 24)
-// );
-// console.log(
-//   `Let's subtrackt the days and hours from that numbers: 13 days is ${
-//     13 * 24
-//   } hours. So let's take that from the hour difference, so that we can say it's 13 days and: ${
-//     babyDueDate.getTime() / 1000 / 60 / 60 -
-//     currentTime.getTime() / 1000 / 60 / 60 -
-//     currentTime.getTime() / 1000 / 60 / 60
-//   } hours to birth `
-// );
-// console.log(
-//   `Next we'll subtract the hours. Now it's 9 hours, which is 9*60 minutes, meaning ${
-//     9 * 60
-//   } minutes`
-// );
+//Let's get countdown to users input:
+
+let inputBtn = document
+  .getElementById("input")
+  .addEventListener("click", () => {
+    let inputDate = document.getElementById("userDate").value;
+    console.log(inputDate);
+    let userDate = new Date(inputDate).getTime();
+    let eventName = document.getElementById("eventName").value;
+    console.log(userDate); //checking everything work so far
+    console.log(eventName);
+
+    let interval2 = setInterval(function () {
+      //making a new interval for user input countdown
+      let currentTime = new Date().getTime();
+
+      let difference = userDate - currentTime;
+
+      let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      let hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      document.getElementById("countdown2").innerHTML =
+        days +
+        " days, " +
+        hours +
+        " hours, " +
+        minutes +
+        " minutes and " +
+        seconds +
+        " seconds until " +
+        eventName +
+        ". That includes " +
+        (days - Math.floor(days / 7) * 2 + " weekdays.");
+    });
+    let reset = document //making functionality to clear countdown to make a new one without refreshing
+      .getElementById("clear")
+      .addEventListener("click", () => {
+        document.getElementById("myForm").reset();
+        clearInterval(interval2);
+        document.getElementById("countdown2").innerHTML = "";
+      });
+  });
